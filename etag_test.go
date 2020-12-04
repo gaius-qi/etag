3d839be9fdceb3613d6dfcec1ffe99024b176c96
gaius.qi <gaius@qiwenbo.com>
2020-12-04 23:04:16 +0800

    test: rm entitys

--- etag_test.go
+++ etag_test.go
@@ -8,7 +8,6 @@ func TestGenerateEtag(t *testing.T) {
 	entitys := []string{"etag", ""}
 	etags := []string{
 		"\"4-92e200311a56800b3e475bf2d2442724535e87bf\"",
-		"\"6-ae0a7afecee1e83d27e322e287cb9bf351ea0018\"",
 		"\"0-da39a3ee5e6b4b0d3255bfef95601890afd80709\"",
 	}
 
@@ -24,7 +23,6 @@ func TestGenerateWeekEtag(t *testing.T) {
 	entitys := []string{"etag", ""}
 	etags := []string{
 		"W/\"4-92e200311a56800b3e475bf2d2442724535e87bf\"",
-		"W/\"6-ae0a7afecee1e83d27e322e287cb9bf351ea0018\"",
 		"W/\"0-da39a3ee5e6b4b0d3255bfef95601890afd80709\"",
 	}
 
